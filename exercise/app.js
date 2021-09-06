a08c0ec29af80d59c51f02250e3214e13809b4be
Wook <wooke1995@gmail.com>
2021-09-06 11:00:01 +0900

    add document elements

--- exercise/app.js
+++ exercise/app.js
@@ -1,1 +1,11 @@
-console.log();
+const title = document.getElementById("title");
+const hellos = document.getElementsByClassName("some");
+const query = document.querySelector(".something h1");
+
+title.innerText = "Got you";
+hellos[0].innerText = "Test you";
+query.innerText = "Query you";
+
+// console.log(title);
+// console.log(hellos);
+// console.log(query);
