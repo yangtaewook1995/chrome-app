db7b50f3e32d5b9f225b30132b41ce16e0121dd4
Wook <wooke1995@gmail.com>
2021-09-06 11:12:38 +0900

    add event listener

--- exercise/app.js
+++ exercise/app.js
@@ -4,8 +4,17 @@ const query = document.querySelector(".something h1");
 
 title.innerText = "Got you";
 hellos[0].innerText = "Test you";
-query.innerText = "Query you";
+query.innerText = "Query too";
 
 // console.log(title);
 // console.log(hellos);
 // console.log(query);
+
+function handelQueryClick() {
+  console.log("title clicked");
+  query.innerText = "Query you";
+}
+
+query.style.color = "blue";
+
+query.addEventListener("click", handelQueryClick);
